600e92b14f80b6aa24cf8318ef84ee79885c51e0
estebancb03 <estebanr.castaneda03@gmail.com>
2022-03-09 22:26:38 -0600

    Function to register users was created

--- backend/controllers/veterinaryController.js
+++ backend/controllers/veterinaryController.js
@@ -1,10 +1,25 @@
+import mongoose from "mongoose";
+import Veterinary from "../models/Veterinary.js";
 
-const toRegister = (req, res) => {
-    res.send('api/veterinarians');
+const toRegister = async (req, res) => {
+    const { email } = req.body;
+    //To prevent duplicated users
+    const userExist = await Veterinary.findOne({ email });
+    if(userExist) {
+        const error = new Error('This user already exists');
+        return res.status(400).json({ message: error.message });
+    }
+    try {
+        const veterinary = new Veterinary(req.body);
+        const savedVeterinary = await veterinary.save();
+        res.json(savedVeterinary);
+    } catch(exception) {
+        console.error(exception);
+    }
 }
 
 const profile = (req, res) => {
-    res.send('api/veterinarians/profile');
+    res.json({ message: 'Showing profile...' });
 }
 
 export {
