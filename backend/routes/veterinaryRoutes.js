75e17ea79b6597d77e5979b4568d0dc8667fa7b6
estebancb03 <estebanr.castaneda03@gmail.com>
2022-03-13 00:09:09 -0600

    Routes were separed in public and private

--- backend/routes/veterinaryRoutes.js
+++ backend/routes/veterinaryRoutes.js
@@ -5,11 +5,14 @@ import {
     toConfirm,
     authenticate
 } from '../controllers/veterinaryController.js';
+import checkAuthentication from '../middleware/authenticationMiddleware.js'
 
 const router = express.Router();
+//Public access routes
 router.post('/', toRegister);
-router.get('/profile', profile);
 router.get('/confirm/:token', toConfirm);
 router.post('/login', authenticate);
+//Private access routes
+router.get('/profile', checkAuthentication, profile);
 
 export default router;
