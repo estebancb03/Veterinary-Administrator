5a95fffe41fdc904fa8d92295bad7b2c1620b0ff
estebancb03 <estebanr.castaneda03@gmail.com>
2022-03-14 15:16:59 -0600

    Routes dependences were installed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,14 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <h1>Hello world</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route>
+          
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
