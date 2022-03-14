2938c44e9f2a5c994f5fb4fcc663749591d3be31
estebancb03 <estebanr.castaneda03@gmail.com>
2022-03-14 00:24:10 -0600

    Method that updates a patient was created

--- backend/controllers/patientController.js
+++ backend/controllers/patientController.js
@@ -14,6 +14,10 @@ const addPatient = async (req, res) => {
 const getPatient = async (req, res) => {
     const { id } = req.params;
     const patient = await Patient.findById(id);
+    if(!patient) {
+        const error = new Error('Patient not found');
+        res.status(404).json({ message: error.message });
+    }
     if(patient.veterinary._id.toString() !== req.veterinary._id.toString()) {
         const error = new Error('Invalid action');
         return res.json({ message: error.message });
@@ -26,8 +30,33 @@ const getPatients = async (req, res) => {
     res.json(patients);
 }
 
+const updatePatient = async (req, res) => {
+    const { id } = req.params;
+    const patient = await Patient.findById(id);
+    if(!patient) {
+        const error = new Error('Patient not found');
+        res.status(404).json({ message: error.message });
+    }
+    if(patient.veterinary._id.toString() !== req.veterinary._id.toString()) {
+        const error = new Error('Invalid action');
+        return res.json({ message: error.message });
+    }
+    patient.name = req.body.name || patient.name;
+    patient.owner = req.body.owner || patient.owner;
+    patient.email = req.body.email || patient.email;
+    patient.date = req.body.date || patient.date;
+    patient.sympthom = req.body.sympthom || patient.sympthom;
+    try {
+        const updatedPatient = await patient.save();
+        res.json(updatePatient);
+    } catch(exception) {
+        console.error(exception);
+    }
+}
+
 export { 
     addPatient, 
     getPatient,
-    getPatients
+    getPatients,
+    updatePatient
 }
