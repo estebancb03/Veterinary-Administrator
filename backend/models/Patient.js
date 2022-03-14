7eff984febf3cb03fe1dc0ca0eedba176aa7e625
estebancb03 <estebanr.castaneda03@gmail.com>
2022-03-13 22:03:02 -0600

    Patient's model was created

---
+++ backend/models/Patient.js
@@ -0,0 +1,36 @@
+import mongoose from 'mongoose';
+
+const patientSchema = mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true
+        },
+        owner: {
+            type: String,
+            required: true
+        },
+        email: {
+            type: String,
+            required: true
+        },
+        date: {
+            type: Date,
+            required: true
+        },
+        sympthom: {
+            type: String,
+            reqired: true
+        },
+        veterinary: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Veterinary'
+        }
+    }, 
+    {
+        timestamps: true,
+    }
+);
+
+const Patient = mongoose.model('Patient', patientSchema);
+export default Patient;
