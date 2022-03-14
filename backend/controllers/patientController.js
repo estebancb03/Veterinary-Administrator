1c340b48603947ed6c0dc18814f1b8646e75b823
estebancb03 <estebanr.castaneda03@gmail.com>
2022-03-13 23:26:35 -0600

    Method that gets one especific patient was created

--- backend/controllers/patientController.js
+++ backend/controllers/patientController.js
@@ -11,9 +11,23 @@ const addPatient = async (req, res) => {
     }
 }
 
+const getPatient = async (req, res) => {
+    const { id } = req.params;
+    const patient = await Patient.findById(id);
+    if(patient.veterinary._id.toString() !== req.veterinary._id.toString()) {
+        const error = new Error('Invalid action');
+        return res.json({ message: error.message });
+    }
+    res.json(patient);
+}
+
 const getPatients = async (req, res) => {
     const patients = await Patient.find().where('veterinary').equals(req.veterinary);
     res.json(patients);
 }
 
-export { addPatient, getPatients }
+export { 
+    addPatient, 
+    getPatient,
+    getPatients
+}
