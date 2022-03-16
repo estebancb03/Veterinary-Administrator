a080938fa41045d5bf55c4a262b468e7a549e768
estebancb03 <estebanr.castaneda03@gmail.com>
2022-03-16 00:41:49 -0600

    Starting with the method to send a email

---
+++ backend/helpers/emailRegistration.js
@@ -0,0 +1,14 @@
+import nodemailer from 'nodemailer';
+
+const emailRegistration = () => {
+    const transport = nodemailer.createTransport({
+        host: process.env.EMAIL_HOST,
+        port: process.env.EMAIL_PORT,
+        auth: {
+          user: process.env.EMAIL_USER,
+          pass: process.env.EMAIL_PASSWORD
+        }
+    });
+}
+
+export default emailRegistration;
