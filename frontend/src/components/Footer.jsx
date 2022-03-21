390043d3dd46cf0b2f6105d88cfef6c6f5f6a3c7
estebancb03 <estebanr.castaneda03@gmail.com>
2022-03-20 23:48:22 -0600

    Footer and Header were created

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+    return (
+        <footer className="py-10">
+            <p className="text-center font-bold">VPA - Veterinary Patients 
+            { '' }
+                <span className="text-indigo-600"> Administrator</span>
+            </p>
+        </footer>
+    );
+}
+
+export default Footer;
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,25 @@
+import { Link } from "react-router-dom";
+
+const Header = () => {
+    return (
+        <header className="py-10 bg-indigo-600">
+            <div className="container mx-auto flex flex-col lg:flex-row justify-between items-center">
+                <h1 className="font-bold text-2xl text-indigo-200 text-center">Veterinary Patientes { '' } 
+                    <span className="text-white font-black"> Administrator</span>
+                </h1>
+                <nav className="flex flex-col lg:flex-row items-center gap-4 mt-5 lg:mt-0">
+                    <Link to='/admin' className='text-white text-sm uppercase font-bold'>Patients</Link>
+                    <Link to='/admin' className='text-white text-sm uppercase font-bold'>Profile</Link>
+                    <button
+                        type="button"
+                        className="text-white text-sm uppercase font-bold"
+                    >
+                        Log out
+                    </button>
+                </nav>
+            </div>
+        </header>
+    );
+}
+
+export default Header;
